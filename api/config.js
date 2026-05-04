875b5ec0ebca7c379cf573fc15db09b838ac70a3
0hankhank0 <gread3265@gmail.com>
2026-05-04 21:53:37 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,9 +1,10 @@
-﻿module.exports = async (req, res) => {
+export default function handler(req, res) {
   res.setHeader("Content-Type", "application/javascript; charset=utf-8");
   res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate=600");
 
-  const publicToken = process.env.MAPBOX_PUBLIC_TOKEN || "";
-  const payload = JSON.stringify(publicToken);
+  const token = process.env.MAPBOX_PUBLIC_TOKEN || "";
 
-  return res.status(200).send(`window.MAPBOX_ACCESS_TOKEN = ${payload};`);
-};
+  res.status(200).send(`
+    window.MAPBOX_TOKEN = "${token}";
+  `);
+}
